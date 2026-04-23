29d8a6f1f99138522365dcce553c00fc4e23eea2
Ammar Ahmed <152694573+Sodaguyprime@users.noreply.github.com>
2026-04-23 12:13:13 +0400

    Add basic arithmetic functions in main.js

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const Add = (num1,num2) => {
+    console.log(num1+ num2)
+}
+
+const Subtract = () => {
+    console.log(num1 - num2)
+}
+
+const Multiply = () => {
+    console.log(num1 * num2)
+}
+
+const Divide = () => {
+    console.log(num1 / num2);
+}
+
+const Operate = (operation) => { 
+
+}
